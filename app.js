6220214d2758ede48e8761689d8fa97e19feae6c
unknown <ajaymedidhi858.com>
2024-10-25 20:10:56 +0530

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require('express'); 
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const patientResourceRoutes = require('./routes/patientResourceRoutes');
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json()); // Middleware to parse JSON bodies 
+app.use(cors({
+    origin: 'https://annphysiocare.com' // Allow only your website
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
